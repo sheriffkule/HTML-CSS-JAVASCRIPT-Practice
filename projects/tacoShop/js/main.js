bdb6d29307e9ec387c2c6f13dd44f9e22ecd6d7d
sheriffkule <zeljko225@yahoo.com>
2024-08-10 23:24:12 +0200

    Little Taco Shop

---
+++ projects/tacoShop/js/main.js
@@ -0,0 +1,35 @@
+const year = document.getElementById('year');
+const thisYear = new Date().getFullYear();
+year.setAttribute('datetime', thisYear);
+year.textContent = thisYear;
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () =>
+	document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () =>
+	themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+
+if (selectedTheme) {
+	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+		darkTheme
+	);
+	themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](
+		iconTheme
+	);
+} else {
+	document.body.classList.add(darkTheme);
+	themeButton.classList.add(iconTheme);
+}
+themeButton.addEventListener('click', () => {
+	document.body.classList.toggle(darkTheme);
+	themeButton.classList.toggle(iconTheme);
+
+	localStorage.setItem('selected-theme', getCurrentTheme());
+	localStorage.setItem('selected-icon', getCurrentIcon());
+});
