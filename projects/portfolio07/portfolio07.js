74f4a52b3791aaffef3e07e74a605bdbb9eff08a
sheriffkule <zeljko225@yahoo.com>
2024-11-18 14:23:13 +0100

    Personal Portfolio part 1

---
+++ projects/portfolio07/portfolio07.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.input', {
+	strings: ['Coder.', 'Frontend Developer.', 'UI/UX Designer.', 'Web Developer.'],
+	typeSpeed: 300,
+	backSpeed: 50,
+	loop: true,
+	loopCount: Infinity,
+	showCursor: true,
+	cursorChar: '|',
+	smartBackspace: true,
+});
