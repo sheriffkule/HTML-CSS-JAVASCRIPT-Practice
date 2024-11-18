4d684c861d732bda5f4291e40c558fab5d4816e2
sheriffkule <zeljko225@yahoo.com>
2024-11-18 18:56:34 +0100

    Personal Portfolio part 2

--- projects/portfolio07/portfolio07.js
+++ projects/portfolio07/portfolio07.js
@@ -1,5 +1,10 @@
 var typed = new Typed('.input', {
-	strings: ['Coder.', 'Frontend Developer.', 'UI/UX Designer.', 'Web Developer.'],
+	strings: [
+		'Coder.',
+		'Frontend Developer.',
+		'UI/UX Designer.',
+		'Web Developer.',
+	],
 	typeSpeed: 300,
 	backSpeed: 50,
 	loop: true,
@@ -7,4 +12,22 @@ var typed = new Typed('.input', {
 	showCursor: true,
 	cursorChar: '|',
 	smartBackspace: true,
+});
+
+const resumeBtns = document.querySelectorAll('.resume-btn');
+
+resumeBtns.forEach((btn, idx) => {
+    btn.addEventListener('click', () => {
+        const resumeDetails = document.querySelectorAll('.resume-detail');
+
+		resumeBtns.forEach((btn) => {
+			btn.classList.remove('active');
+        });
+        btn.classList.add('active');
+
+        resumeDetails.forEach(detail => {
+            detail.classList.remove('active');
+        });
+        resumeDetails[idx].classList.add('active');
+	});
 });
