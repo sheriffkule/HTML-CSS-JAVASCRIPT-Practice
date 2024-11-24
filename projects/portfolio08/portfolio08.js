fc7959cd7fd81d9744fa77a71310983b2f8095f0
sheriffkule <zeljko225@yahoo.com>
2024-11-24 09:59:03 +0100

    3D Portfolio Website part 3

--- projects/portfolio08/portfolio08.js
+++ projects/portfolio08/portfolio08.js
@@ -1,12 +1,24 @@
 const resumeList = document.querySelectorAll('.resume-list');
-const resumeBoxes = document.querySelectorAll('.resume-box')
+const resumeBoxes = document.querySelectorAll('.resume-box');
+const portfolioList = document.querySelectorAll('.portfolio-list');
+const portfolioBoxes = document.querySelectorAll('.portfolio-box');
 
 resumeList.forEach((list, idx) => {
-    list.addEventListener('click', () => {
-        document.querySelector('.resume-list.active').classList.remove('active');
-        list.classList.add('active');
+	list.addEventListener('click', () => {
+		document.querySelector('.resume-list.active').classList.remove('active');
+		list.classList.add('active');
 
-        document.querySelector('.resume-box.active').classList.remove('active');
-        resumeBoxes[idx].classList.add('active')
-    })
-})
+		document.querySelector('.resume-box.active').classList.remove('active');
+		resumeBoxes[idx].classList.add('active');
+	});
+});
+
+portfolioList.forEach((list, idx) => {
+	list.addEventListener('click', () => {
+		document.querySelector('.portfolio-list.active').classList.remove('active');
+		list.classList.add('active');
+
+        document.querySelector('.portfolio-box.active').classList.remove('active');
+		portfolioBoxes[idx].classList.add('active');
+	});
+});
