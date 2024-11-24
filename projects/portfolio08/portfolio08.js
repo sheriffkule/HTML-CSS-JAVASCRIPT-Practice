b3d80b9e6e8e9695b7a24286834b135101fe6694
sheriffkule <zeljko225@yahoo.com>
2024-11-24 11:34:08 +0100

    3D Portfolio Website

--- projects/portfolio08/portfolio08.js
+++ projects/portfolio08/portfolio08.js
@@ -1,7 +1,33 @@
+const navs = document.querySelectorAll('.nav-list li');
+const cube = document.querySelector('.box');
+const sections = document.querySelectorAll('.section');
 const resumeList = document.querySelectorAll('.resume-list');
 const resumeBoxes = document.querySelectorAll('.resume-box');
 const portfolioList = document.querySelectorAll('.portfolio-list');
 const portfolioBoxes = document.querySelectorAll('.portfolio-box');
+
+navs.forEach((nav, idx) => {
+	nav.addEventListener('click', () => {
+		document.querySelector('.nav-list li.active').classList.remove('active');
+		nav.classList.add('active');
+
+        cube.style.transform = `rotateY(${idx * -90}deg)`;
+
+        document.querySelector('.section.active').classList.remove('active');
+		sections[idx].classList.add('active');
+
+        const array = Array.from(sections);
+        const arrSecs = array.slice(1, -1);
+        arrSecs.forEach(arrSecs => {
+            if (arrSecs.classList.contains('active')) {
+                sections[4].classList.add('action-contact');
+            }
+        });
+        if (sections[0].classList.contains('active')) {
+            sections[4].classList.remove('action-contact');
+        }
+	});
+});
 
 resumeList.forEach((list, idx) => {
 	list.addEventListener('click', () => {
@@ -22,3 +48,7 @@ portfolioList.forEach((list, idx) => {
 		portfolioBoxes[idx].classList.add('active');
 	});
 });
+
+setTimeout(() => {
+    sections[4].classList.remove('active')
+}, 1500);
