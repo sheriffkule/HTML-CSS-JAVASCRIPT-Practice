1d5ffca8ae036814031054e90d63d461f9883874
sheriffkule <zeljko225@yahoo.com>
2025-01-18 15:46:57 +0100

    Home & Header Section

--- projects/restaurantWebsite/restaurantWebsite.js
+++ projects/restaurantWebsite/restaurantWebsite.js
@@ -1,5 +1,13 @@
 let homeSwiper = new Swiper('.home-swiper', {
+  spaceBetween: 30,
+  loop: 'true',
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
 });
