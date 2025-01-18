44a5704d008ff6fece9a1f1c8609390a17cc05fc
sheriffkule <zeljko225@yahoo.com>
2025-01-18 21:43:24 +0100

    Services & Deals section

--- projects/restaurantWebsite/restaurantWebsite.js
+++ projects/restaurantWebsite/restaurantWebsite.js
@@ -9,5 +9,25 @@ let homeSwiper = new Swiper('.home-swiper', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  }
+  },
+});
+
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[content]');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('active-tab');
+    });
+
+    target.classList.add('active-tab');
+
+    tabs.forEach((tab) => {
+        tab.classList.remove('active-tab');
+    });
+    
+    tab.classList.add('active-tab');
+  });
 });
