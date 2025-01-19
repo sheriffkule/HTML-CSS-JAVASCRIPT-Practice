de6b97898987f73958e514a786fc4670420c68ba
sheriffkule <zeljko225@yahoo.com>
2025-01-19 16:53:50 +0100

    Restaurant Website Finished

--- projects/restaurantWebsite/restaurantWebsite.js
+++ projects/restaurantWebsite/restaurantWebsite.js
@@ -1,3 +1,50 @@
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+	navMenu.classList.remove('show-menu');
+};
+
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+	const scrollDown = window.scrollY;
+
+	sections.forEach((current) => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 58;
+		const sectionId = current.getAttribute('id');
+		const sectionClass = document.querySelector(
+			'.nav__menu a[href*=' + sectionId + ']'
+		);
+
+		if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+			sectionClass.classList.add('active-link');
+		} else {
+			sectionClass.classList.remove('active-link');
+		}
+	});
+};
+
+window.addEventListener('scroll', scrollActive);
+
 let homeSwiper = new Swiper('.home-swiper', {
   spaceBetween: 30,
   loop: 'true',
@@ -25,22 +72,35 @@ tabs.forEach((tab) => {
     target.classList.add('active-tab');
 
     tabs.forEach((tab) => {
-        tab.classList.remove('active-tab');
+      tab.classList.remove('active-tab');
     });
-    
+
     tab.classList.add('active-tab');
   });
 });
 
 const scrollUp = () => {
-	const scrollUp = document.getElementById('scroll-up');
+  const scrollUp = document.getElementById('scroll-up');
 
-	this.scrollY >= 350
-		? scrollUp.classList.add('show-scroll')
-		: scrollUp.classList.remove('show-scroll');
+  this.scrollY >= 350
+    ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll');
 };
 
 window.addEventListener('scroll', scrollUp);
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '200px',
+  duration: 2000,
+  delay: 300,
+  mobile: true,
+});
+
+sr.reveal(`.home__data, .section__group, .section__container, .menu__item, .about__item`, {interval: 200})
+sr.reveal(`.services__item, .footer__content`, {origin: 'bottom', interval: 200})
+sr.reveal(`.title-center, .tab__btns, .section__title, .section__subtitle, .reservation__info`, {origin: 'left', interval: 200})
+sr.reveal(`.tab__item, .reservation__form`, {origin: 'right'})
 
 const year = document.getElementById('year');
 const thisYear = new Date().getFullYear();
