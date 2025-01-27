c7e0733e7e3e50bbd50b0c9529da23d24a8b7248
sheriffkule <zeljko225@yahoo.com>
2025-01-28 00:08:15 +0100

    Food Website part 6

--- projects/foodWebsite/foodWebsite.js
+++ projects/foodWebsite/foodWebsite.js
@@ -1,6 +1,34 @@
 $(document).ready(function ($) {
   'use strict';
 
+  let book_table = new Swiper(".book-table-img-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    speed: 2000,
+    effect: 'coverflow',
+    coverflowEffect: {
+      rotate: 3,
+      stretch: 2,
+      depth: 100,
+      modifier: 5,
+      slideShadows: false,
+    },
+    loopAdditionSlides: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
   jQuery('.filters').on('click', function () {
     jQuery('#menu-dish').removeClass('byDefault_show');
   });
