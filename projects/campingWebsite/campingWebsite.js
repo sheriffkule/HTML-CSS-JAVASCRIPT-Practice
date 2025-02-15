b6c24203fedc923eb4daa9844b8f2293b4dcd4ae
SheriffKule <zeljko225@yahoo.com>
2025-02-15 10:51:48 +0100

    Camping Website part 3

--- projects/campingWebsite/campingWebsite.js
+++ projects/campingWebsite/campingWebsite.js
@@ -35,3 +35,35 @@ let swiper = new Swiper('.categorySwiper', {
     },
   },
 });
+
+let swiper2 = new Swiper('.selling-slider', {
+  loop: true,
+  parallax: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    280: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    510: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    750: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    920: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
