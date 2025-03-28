996e15a882a9d875dc4228d9c5ded035d73ed679
SheriffKule <zeljko225@yahoo.com>
2025-03-28 09:39:43 +0100

    Amazing Flip Card Finished

--- box_script/boxBorderAnimation71.js
+++ box_script/boxBorderAnimation71.js
@@ -27,6 +27,68 @@ cards.forEach((card) => {
   card.addEventListener('mousemove', (e) => {
     if (!card.classList.contains('flipped')) {
       const { left, top } = card.getBoundingClientRect();
+      const x = e.clientX - left;
+      const y = e.clientY - top;
+
+      const centerX = card.offsetWidth / 2;
+      const centerY = card.offsetHeight / 2;
+
+      const angelX = (y - centerX) / 30;
+      const angelY = (centerX - x) / 30;
+
+      cardInner.style.transform = `rotateX(${angelX}deg) rotateY(${angelY}deg)`;
+
+      shine.style.background = `linear-gradient(${Math.atan2(y - centerY, x - centerX) * (180 / Math.PI)}deg,
+        rgba(255, 255, 255, 0.25) 0%, rgba(255, 255, 255, 0) 60%)`;
+    }
+  });
+
+  card.addEventListener('mouseleave', () => {
+    if (!card.classList.contains('flipped')) {
+      cardInner.style.transform = '';
+    } else {
+      cardInner.style.transform = 'rotateY(180deg)';
     }
   });
 });
+
+function createParticles() {
+  const particles = document.getElementById('particles');
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+
+  particles.innerHTML = '';
+
+  for (let i = 0; i < 120; i++) {
+    const size = Math.random() * 10 + 2;
+
+    const left = Math.random() * windowWidth;
+    const top = Math.random() * windowHeight;
+
+    const delay = Math.random() * 20;
+    const duration = Math.random() * 15 + 10;
+
+    const particle = document.createElement('span');
+    particle.classList.add('particle');
+
+    particle.style.width = `${size}px`;
+    particle.style.height = `${size}px`;
+    particle.style.left = `${left}px`;
+    particle.style.top = `${top}px`;
+    const moveDirection = Math.floor(Math.random() * 4);
+    const moveX = Math.random() * 300 - 10 + 'px';
+    const moveY = Math.random() * 300 - 10 + 'px';
+
+    particle.style.setProperty('--move-x', moveX);
+    particle.style.setProperty('--move-y', moveY);
+
+    const animations = ['floatUp', 'floatDown', 'floatLeft', 'floatRight'];
+    particle.style.animation = `${animations[moveDirection]} ${duration}s linear ${delay}s infinite`;
+
+    particles.appendChild(particle);
+  }
+}
+
+createParticles();
+
+window.addEventListener('resize', createParticles);
