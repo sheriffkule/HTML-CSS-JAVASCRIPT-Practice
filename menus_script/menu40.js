8cb3760bbe8d4e5098d0791431c4e2a1ae1438a0
SheriffKule <zeljko225@yahoo.com>
2025-04-09 20:03:50 +0200

    Animated Menu finished

--- menus_script/menu40.js
+++ menus_script/menu40.js
@@ -2,5 +2,26 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 
 menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('change');
-})
+  menu.classList.toggle('change');
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target !== menuBtn && !menu.contains(e.target)) {
+    menu.classList.remove('change');
+  }
+});
+
+const navLinks = [...document.querySelectorAll('.nav-link')];
+
+navLinks.forEach((link, i) => {
+  link.textContent.split('').forEach((letter) => {
+    const span = document.createElement('span');
+    span.classList.add('letter');
+    span.textContent = letter;
+    link.appendChild(span);
+  });
+  link.addEventListener('click', () => {
+    menu.classList.remove('change');
+  });
+  link.removeChild(link.childNodes[0]);
+});
