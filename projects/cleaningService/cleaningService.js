c64b18edfa14df35cf1ea8adc145a26de63d3aaa
SheriffKule <zeljko225@yahoo.com>
2025-04-16 22:43:15 +0200

    Cleaning Service Website part 5

--- projects/cleaningService/cleaningService.js
+++ projects/cleaningService/cleaningService.js
@@ -1,6 +1,21 @@
 const toggle = document.getElementById('toggle');
 const menu = document.querySelector('.menu');
 
-toggle.addEventListener('click', function() {
-    menu.classList.toggle('active');
-})
+toggle.addEventListener('click', function () {
+  menu.classList.toggle('active');
+});
+
+let swiper = new Swiper('.testimonials_swiper', {
+  loop: false,
+  grabCursor: true,
+  slidesPerView: 2,
+  spaceBetween: 30,
+  breakpoints: {
+    1000: {
+      slidesPerView: 2  
+    },
+    0: {
+        slidesPerView: 1
+    }
+  }
+});
