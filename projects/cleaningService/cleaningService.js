73536f5eeeadd466488e9ed52dfb7e2b74d7ad25
SheriffKule <zeljko225@yahoo.com>
2025-04-19 17:50:29 +0200

    Cleaning Service Website Finished

--- projects/cleaningService/cleaningService.js
+++ projects/cleaningService/cleaningService.js
@@ -12,27 +12,27 @@ let swiper = new Swiper('.testimonials_swiper', {
   spaceBetween: 30,
   breakpoints: {
     1000: {
-      slidesPerView: 2  
+      slidesPerView: 2,
     },
     0: {
-        slidesPerView: 1
-    }
-  }
+      slidesPerView: 1,
+    },
+  },
 });
 
-document.querySelectorAll('.faq_item').forEach(item => {
+document.querySelectorAll('.faq_item').forEach((item) => {
   const question = item.querySelector('.faq_question');
   const answer = item.querySelector('.faq_answer');
   const icon = question.querySelector('i');
 
-  question.addEventListener('click', function() {
+  question.addEventListener('click', function () {
     const isActive = item.classList.toggle('active');
 
-    document.querySelectorAll('.faq_item').forEach(otherItem => {
-      if(otherItem != item) {
+    document.querySelectorAll('.faq_item').forEach((otherItem) => {
+      if (otherItem != item) {
         otherItem.classList.remove('active');
         otherItem.querySelector('.faq_answer').style.maxHeight = null;
-        otherItem.querySelector('.faq_question i').classList.replace('fa-minus', 'fa-plus')
+        otherItem.querySelector('.faq_question i').classList.replace('fa-minus', 'fa-plus');
       }
     });
 
@@ -44,4 +44,23 @@ document.querySelectorAll('.faq_item').forEach(item => {
       icon.classList.replace('fa-minus', 'fa-plus');
     }
   });
+});
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
 })
+
+function updateYear() {
+  const currentYear = new Date().getFullYear();
+  const yearElement = document.getElementById('year');
+  yearElement.dateTime = currentYear;
+  yearElement.textContent = currentYear;
+}
+
+updateYear();
