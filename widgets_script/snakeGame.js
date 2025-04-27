4b04ebc87f832af29558e5d9da1cb8da43dc4ef2
SheriffKule <zeljko225@yahoo.com>
2025-04-27 11:51:01 +0200

    Snake Game more functionality

--- widgets_script/snakeGame.js
+++ widgets_script/snakeGame.js
@@ -3,6 +3,7 @@ const scoreElement = document.querySelector('.score');
 const highScoreElement = document.querySelector('.high-score');
 const controls = document.querySelectorAll('.controls i');
 const skinContainer = document.getElementById('skin-container');
+const foodColorContainer = document.getElementById('food-color-container');
 
 let gameOver = false;
 let foodX = 18;
@@ -14,6 +15,8 @@ let snakeBody = [];
 let velocityY = 0;
 let setIntervalId;
 let score = 0;
+let head = '#60cbff';
+let foodColor = 'red';
 
 let highScore = localStorage.getItem('high-score') || 0;
 highScoreElement.innerText = `High Score: ${highScore * 10}`;
@@ -66,7 +69,7 @@ controls.forEach((key) => {
 
 const initGame = () => {
   if (gameOver) return handleGameOver();
-  let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+  let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}; background-color: ${foodColor};"></div>`;
 
   if (snakeX === foodX && snakeY === foodY) {
     changeFoodPosition();
@@ -107,40 +110,46 @@ setIntervalId = setInterval(initGame, 180);
 document.addEventListener('keydown', changeDirection);
 
 const pauseElement = document.createElement('div');
-pauseElement.classList.toggle('paused')
+pauseElement.classList.toggle('paused');
 pauseElement.textContent = 'PAUSED';
 
 document.body.appendChild(pauseElement);
 
 document.addEventListener('keydown', (e) => {
+  if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+  }
+  changeDirection(e);
   if (e.key === ' ') {
     if (setIntervalId) {
       clearInterval(setIntervalId);
       setIntervalId = null;
       pauseElement.style.display = 'block';
     } else {
-      setIntervalId = setInterval(initGame, 175);
+      setIntervalId = setInterval(initGame, 180);
       pauseElement.style.display = 'none';
     }
   }
 });
 
 const snakeSkins = [
-  { name: 'Default', color: '#60cbff'},
+  { name: 'Default', color: '#60cbff' },
   { name: 'Green', color: 'rgb(38,217,59)' },
   { name: 'Pink', color: 'rgb(242,13,175)' },
   { name: 'Blue', color: 'rgb(0,30,255)' },
   { name: 'Yellow', color: 'rgb(242,255,0)' },
   { name: 'Silver', color: 'silver' },
-  { name: 'White', color: 'white'},
-  { name: 'Black', color: 'black'},
-  { name: 'Transparent', color: 'transparent'}
+  { name: 'White', color: 'white' },
+  { name: 'Black', color: 'black' },
+  { name: 'Transparent', color: 'transparent' },
 ];
 
 function createSkinButton(skin) {
   const button = document.createElement('button');
   button.classList.add('btn');
-  button.setAttribute('data-ripple', " ")
+  button.setAttribute('data-ripple', ' ');
   const buttonSpan = document.createElement('span');
   buttonSpan.textContent = skin.name;
   button.appendChild(buttonSpan);
@@ -156,6 +165,7 @@ function updateHeadElements(color) {
   headElements.forEach((head) => {
     head.style.backgroundColor = color;
   });
+
   requestAnimationFrame(() => {
     updateHeadElements(color);
   });
@@ -166,15 +176,72 @@ snakeSkins.forEach((skin) => {
   skinContainer.appendChild(button);
 });
 
+const foodColors = [
+  { name: 'Default', color: foodColor },
+  { name: 'Green', color: 'rgb(38,217,59)' },
+  { name: 'Pink', color: 'rgb(242,13,175)' },
+  { name: 'Blue', color: 'rgb(0,30,255)' },
+  { name: 'Yellow', color: 'rgb(242,255,0)' },
+  { name: 'Silver', color: 'silver' },
+  { name: 'White', color: 'white' },
+  { name: 'Black', color: 'black' },
+  { name: 'Orange', color: 'orange' },
+];
+
+function updateFoodColor(color) {
+  const foodElement = document.querySelector('.food');
+  if (foodElement) {
+    foodElement.style.backgroundColor = color;
+  } else {
+    console.error('Food element not found.');
+  }
+
+  foodColor = color;
+}
+
+
+function createFoodColorButton(color) {
+  const button = document.createElement('button');
+  button.classList.add('btn');
+  button.setAttribute('data-ripple', ' ');
+  const buttonSpan = document.createElement('span');
+  buttonSpan.textContent = color.name;
+  button.appendChild(buttonSpan);
+  button.style.backgroundColor = color.color;
+  button.addEventListener('click', () => {
+    updateFoodColor(color.color);
+  });
+  return button;
+}
+
+foodColors.forEach((color) => {
+  const button = createFoodColorButton(color);
+  foodColorContainer.appendChild(button);
+});
+
 document.body.addEventListener('mousemove', function (e) {
   if (e.target.matches('.btn[data-ripple]')) {
-      const btn = e.target;
-      const x = e.pageX - btn.offsetLeft;
-      const y = e.pageY - btn.offsetTop;
+    const btn = e.target;
+    const x = e.pageX - btn.offsetLeft;
+    const y = e.pageY - btn.offsetTop;
 
-      btn.style.setProperty('--x', x + 'px');
-      btn.style.setProperty('--y', y + 'px');
+    btn.style.setProperty('--x', x + 'px');
+    btn.style.setProperty('--y', y + 'px');
   }
+});
+
+const skinTitles = document.querySelectorAll('.skin_title');
+
+skinTitles.forEach((title) => {
+  title.addEventListener('click', () => {
+    const skinContainer = title.nextElementSibling;
+    const icon = title.querySelector('i');
+    skinContainer.classList.toggle('visible');
+    icon.classList.toggle('rotate-180');
+    skinContainer.classList.contains('visible')
+      ? (title.style.color = '#4169e1')
+      : (title.style.color = '#b8c6dc');
+  });
 });
 
 function updateYear() {
