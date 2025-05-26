0a486788eef065cee99cb214986a6688fc64eb70
SheriffKule <zeljko225@yahoo.com>
2025-05-26 16:44:21 +0200

    Stock Media Platform part 16

--- projects/stockPlatform/pages/photos/photo_detail.js
+++ projects/stockPlatform/pages/photos/photo_detail.js
@@ -3,6 +3,8 @@
 import { client } from '../../js/api_configure.js';
 import { favorite } from '../../js/favorite.js';
 import { menu } from '../../js/menu.js';
+import { photoCard } from '../../js/photo_card.js';
+import { gridInit, updateGrid } from '../../js/utils/masonry_grid.js';
 import { ripple } from '../../js/utils/ripple.js';
 
 const $rippleElems = document.querySelectorAll('[data-ripple]');
@@ -19,9 +21,9 @@ window.addEventListener('DOMContentLoaded', function () {
 
 const $menuWrapper = document.querySelectorAll('[data-menu-wrapper]');
 
-$menuWrapper.forEach($menuWrapper => {
-    menu($menuWrapper)
-})
+$menuWrapper.forEach(($menuWrapper) => {
+  menu($menuWrapper);
+});
 
 const favoritePhotos = JSON.parse(window.localStorage.getItem('favorite')).photos;
 const $favoriteBtn = document.querySelector('[data-add-favorite]');
@@ -35,13 +37,60 @@ const $detailWrapper = document.querySelector('[data-detail-wrapper]');
 const $downloadLink = document.querySelector('[data-download-link]');
 const $downloadMenu = document.querySelector('[data-download-menu]');
 
-client.photos.detail(photoId, data => {
-    const {
-        avg_color,
-        height,
-        width,
-        photographer,
-        alt,
-        src
-    } = data;
-})
+client.photos.detail(photoId, (data) => {
+  const { avg_color, height, width, photographer, alt, src } = data;
+
+  $downloadLink.href = src.original;
+
+  Object.entries(src).forEach((item) => {
+    const [key, value] = item;
+
+    $downloadMenu.innerHTML += `
+      <a href="${value}" download class="menu-item" data-ripple data-menu-item>
+        <span class="label-large text">${key}</span>
+        <div class="state-layer"></div>
+      </a>
+    `;
+  });
+
+  $detailWrapper.innerHTML = `
+    <figure class="detail-banner" style="aspect-ratio: ${width} / ${height}; background-color: ${avg_color}">
+      <img src="${src.large2x}" width="${width}" height="${height}" alt="${alt}" class="img-cover" />
+    </figure>
+    <p class="title-small">Photograph by <span class="color-primary">${photographer}</span></p>
+  `;
+
+  const $detailImg = $detailWrapper.querySelector('img');
+
+  $detailImg.style.opacity = '0';
+
+  $detailImg.addEventListener('load', function () {
+    this.animate(
+      {
+        opacity: 1,
+      },
+      { duration: 400, fill: 'forwards', easing: 'ease-in' }
+    );
+
+    if (alt) {
+      client.photos.search({ query: alt, page: 1, per_page: 30 }, (data) => {
+        loadSimilar(data);
+      });
+    } else {
+      $$loader.style.display = 'none';
+      $photoGrid.innerHTML = '<p>No similar photos found.</p>';
+    }
+  });
+});
+
+const $photoGrid = document.querySelector('[data-photo-grid]');
+const photoGrid = gridInit($photoGrid);
+const $$loader = document.querySelector('[data-loader]');
+
+const loadSimilar = function (data) {
+  data.photos.forEach((photo) => {
+    const $card = photoCard(photo);
+    updateGrid($card, photoGrid.columnsHeight, photoGrid.$columns);
+    $$loader.style.display = 'none';
+  });
+};
--- projects/stockPlatform/pages/videos/videos.js
+++ projects/stockPlatform/pages/videos/videos.js
@@ -1,6 +1,66 @@
-/**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
- */
-
 "use strict";
+
+'use strict';
+import { client } from '../../js/api_configure.js';
+import { filter } from '../../js/filter.js';
+import { gridInit, updateGrid } from '../../js/utils/masonry_grid.js';
+import { updateUrl } from '../../js/utils/updateUrl.js';
+import { urlDecode } from '../../js/utils/urlDecode.js';
+import { videoCard } from '../../js/video_card.js';
+
+const $filterBar = document.querySelector('[data-filter-bar]');
+
+$filterBar.style.display = window.location.search ? 'flex' : 'none';
+
+const $filterWrappers = document.querySelectorAll('[data-filter]');
+
+$filterWrappers.forEach($filterWrapper => {
+    filter($filterWrapper, window.filterObj, (newObj) => {
+        window.filterObj = newObj;
+        updateUrl(newObj, 'videos');
+    });
+})
+
+const $videoGrid = document.querySelector('[data-video-grid]');
+const $title = document.querySelector('[data-title]');
+const videoGrid = gridInit($videoGrid);
+const perPage = 30;
+let currentPage = 1;
+let totalPage = 0;
+const searchUrl = window.location.search.slice(1);
+let searchObj = searchUrl && urlDecode(searchUrl);
+const title = searchObj ? `${searchObj.query} videos` : 'Popular videos';
+
+$title.textContent = title;
+document.title = title;
+
+const renderVideos = function (currentPage) {
+  client.videos[searchObj ? 'search' : 'popular'](
+    { ...searchObj, per_page: perPage, page: currentPage },
+    (data) => {
+      totalPage = Math.ceil(data.total_results / perPage);
+
+      data.videos.forEach((video) => {
+        const $videoCard = videoCard(video);
+        updateGrid($videoCard, videoGrid.columnsHeight, videoGrid.$columns);
+      });
+
+      isLoading = true;
+
+      if (currentPage >= totalPage) $loader.style.display = 'none';
+    }
+  );
+};
+
+renderVideos(currentPage);
+
+const $loader = document.querySelector('[data-loader]');
+let isLoading = true;
+
+window.addEventListener('scroll', function () {
+  if ($loader.getBoundingClientRect().top < window.innerHeight * 2 && currentPage <= totalPage && isLoading) {
+    currentPage++;
+    renderVideos(currentPage);
+    isLoading = false;
+  }
+});
