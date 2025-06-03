bc8fabb1a68882efc9193563c0e630b7f9d9f03a
SheriffKule <zeljko225@yahoo.com>
2025-06-03 17:31:17 +0200

    Travel Website part 3

--- projects/travelWebsite/travelWebsite.js
+++ projects/travelWebsite/travelWebsite.js
@@ -17,8 +17,31 @@ if (navClose) {
 const navLink = document.querySelectorAll('.nav__link');
 
 const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.remove('show-menu');
-}
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
+};
 
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+const swiperHome = new Swiper('.home__swiper', {
+  loop: true,
+  slidesPerView: 'auto',
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //   autoplay: {
+  //     delay: 3000,
+  //     disableOnInteraction: false,
+  //   }
+});
+
+const bgHeader = () => {
+  const header = document.getElementById('header');
+
+  this.scrollY >= 100 ? header.classList.add('bg-header') : header.classList.remove('bg-header');
+};
+
+window.addEventListener('scroll', bgHeader);
