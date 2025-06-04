5682b5a67352c6a267a608f7e63630c2189ba644
SheriffKule <zeljko225@yahoo.com>
2025-06-04 09:37:03 +0200

    Travel Website part 6

--- projects/travelWebsite/travelWebsite.js
+++ projects/travelWebsite/travelWebsite.js
@@ -58,8 +58,62 @@ const swiperTestimonial = new Swiper('.testimonial__swiper', {
     prevEl: '.swiper-button-prev',
   },
 
-    // autoplay: {
-    //   delay: 3000,
-    //   disableOnInteraction: false,
-    // }
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  // }
+});
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+};
+
+window.addEventListener('scroll', scrollUp);
+
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute('id');
+    const sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+    if (sectionClass) {
+      if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+        sectionClass.classList.add('active-link');
+      } else {
+        sectionClass.classList.remove('active-link');
+      }
+    }
+  });
+};
+
+window.addEventListener('scroll', scrollActive);
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-fill';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? 'dark' : 'light');
+const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? 'ri-moon-fill' : 'ri-sun-fill');
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'ri-sun-fill' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
 });
