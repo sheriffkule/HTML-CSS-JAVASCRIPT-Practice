d0ab97c7a463eea9c442ed64c756dfde5ca07059
SheriffKule <zeljko225@yahoo.com>
2025-06-04 14:12:39 +0200

    Stock Media Platform part 17

--- projects/stockPlatform/pages/collections/collection.js
+++ projects/stockPlatform/pages/collections/collection.js
@@ -1,6 +1,39 @@
-/**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
- */
+'use strict';
 
-"use strict";
+import { client } from '../../js/api_configure.js';
+import { collectionCard } from '../../js/collection_card.js';
+
+const $collectionGrid = document.querySelector('[data-collection-grid]');
+const perPage = 36;
+let currentPage = 1;
+let totalPage = 0;
+
+const loadCollections = function (page) {
+  client.collections.featured({ per_page: perPage, page: page }, (data) => {
+    totalPage = Math.ceil(data.total_results / perPage);
+
+    data.collections.forEach((collection) => {
+      const $collectionCard = collectionCard(collection);
+
+      $collectionGrid.appendChild($collectionCard);
+    });
+
+    isLoaded = true;
+    currentPage >= totalPage && ($loader.style.display = 'none');
+  });
+};
+
+loadCollections(currentPage);
+
+const $loader = document.querySelector('[data-loader]');
+let isLoaded = false;
+
+const loadMore = function () {
+  if ($loader.getBoundingClientRect().top < window.innerHeight * 2 && currentPage <= totalPage && isLoaded) {
+    currentPage++;
+    loadCollections(currentPage);
+    isLoaded = false;
+  }
+};
+
+window.addEventListener('scroll', loadMore);
--- projects/stockPlatform/pages/collections/collection_detail.js
+++ projects/stockPlatform/pages/collections/collection_detail.js
@@ -1,6 +1,41 @@
-/**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
- */
+'use strict';
 
-"use strict";
+import { client } from '../../js/api_configure.js';
+import { photoCard } from '../../js/photo_card.js';
+import { gridInit, updateGrid } from '../../js/utils/masonry_grid.js';
+import { urlDecode } from '../../js/utils/urlDecode.js';
+import { videoCard } from '../../js/video_card.js';
+
+const $collectionGrid = document.querySelector('[data-collection-grid]');
+const $title = document.querySelector('[data-title]');
+const collectionGrid = gridInit($collectionGrid);
+const perPage = 30;
+let currentPage = 1;
+let totalPage = 0;
+const collectionObj = urlDecode(window.location.search.slice(1));
+
+$title.textContent = `${collectionObj.title} collections`;
+document.title = `${collectionObj.title} collections`;
+
+const loadCollection = function (page) {
+  client.collections.detail(collectionObj.collectionId, { per_page: perPage, page: page }, (data) => {
+    totalPage = Math.ceil(data.total_results / perPage);
+
+    data.media.forEach((item) => {
+      let $card;
+
+      switch (item.type.toLowerCase()) {
+        case 'photo':
+          $card = photoCard(item);
+          break;
+        case 'video':
+          $card = videoCard(item);
+          break;
+      }
+
+      updateGrid($card, collectionGrid.columnsHeight, collectionGrid.$columns);
+    });
+  });
+};
+
+loadCollection(currentPage);
