494d7fa427d4d85a4ff3ef49ab9c8264844f32eb
SheriffKule <zeljko225@yahoo.com>
2025-06-05 17:38:44 +0200

    AI Robotics Website part 2

---
+++ projects/AiRoboticsWebsite/AiRoboticsWebsite.js
@@ -0,0 +1,28 @@
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 100) {
+    nav.classList.add('fix_nav');
+  } else {
+    nav.classList.remove('fix_nav');
+  }
+});
+
+const bars = document.querySelector('.bars');
+const secNav = document.querySelector('.second_nav');
+const closeMenu = document.querySelector('.close');
+const nav_btn_show = document.querySelector('.nav_btn_show');
+const nav_icon_menu = document.querySelector('.nav_icon_btn');
+
+bars.addEventListener('click', () => {
+  secNav.classList.toggle('show_sec_nav');
+});
+
+closeMenu.addEventListener('click', () => {
+  secNav.classList.toggle('show_sec_nav');
+});
+
+nav_btn_show.addEventListener('click', () => {
+  nav_icon_menu.classList.toggle('show_nav_icon_btn');
+});
+
