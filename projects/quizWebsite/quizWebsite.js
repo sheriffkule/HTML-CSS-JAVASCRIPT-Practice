078dc42c52f285208067e3cfa88e99745eefda46
SheriffKule <zeljko225@yahoo.com>
2025-06-14 19:06:22 +0200

    Quiz Website part 1

---
+++ projects/quizWebsite/quizWebsite.js
@@ -0,0 +1,68 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Codehal Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hometext Programming",
+            "C. Hypertext Preprogramming",
+            "D. Programming Hypertext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Stylesheet Query Language",
+            "C. Science Question Language",
+            "D. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    }
+];
+
+const startBtn = document.querySelector('.start-btn');
+const popupInfo = document.querySelector('.popup-info');
+const exitBtn = document.querySelector('.exit-btn');
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
+const resultBox = document.querySelector('.result-box');
+const tryAgainBtn = document.querySelector('.tryAgain-btn');
+const goHomeBtn = document.querySelector('.goHome-btn');
