991af75668c0963475986085a488115ead363287
SheriffKule <zeljko225@yahoo.com>
2025-06-15 19:11:05 +0200

    Quiz Website part 4

--- projects/quizWebsite/quizWebsite.js
+++ projects/quizWebsite/quizWebsite.js
@@ -81,4 +81,62 @@ continueBtn.onclick = () => {
   quizSection.classList.add('active');
   popupInfo.classList.remove('active');
   main.classList.remove('active');
+  quizBox.classList.add('active');
+
+  showQuestions(0);
+  questionCounter(1);
 };
+
+let questionCount = 0;
+let questionNumb = 0;
+
+const nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick = () => {
+  if (questionCount < questions.length - 1) {
+    questionCount++;
+    showQuestions(questionCount);
+
+    questionNumb++;
+    questionCounter(questionNumb);
+  } else {
+    console.log('question end');
+  }
+};
+
+const optionList = document.querySelector('.option-list');
+
+function showQuestions(index) {
+  const questionText = document.querySelector('.question-text');
+  questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+  let optionTag = `
+    <div class="option"><span>${questions[index].options[0]}</span></div>
+    <div class="option"><span>${questions[index].options[1]}</span></div>
+    <div class="option"><span>${questions[index].options[2]}</span></div>
+    <div class="option"><span>${questions[index].options[3]}</span></div>
+  `;
+
+  optionList.innerHTML = optionTag;
+
+  const option = document.querySelectorAll('.option');
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute('onclick', 'optionSelected(this)');
+  }
+}
+
+function optionSelected(answer) {
+  let userAnswer = answer.textContent;
+  let correctAnswer = questions[questionCount].answer;
+
+  if (userAnswer == correctAnswer) {
+    console.log('correct');
+  } else {
+    console.log('incorrect');
+  }
+}
+
+function questionCounter(index) {
+  const questionTotal = document.querySelector('.question-total');
+  questionTotal.textContent = `${index} of ${questions.length} Questions`;
+}
