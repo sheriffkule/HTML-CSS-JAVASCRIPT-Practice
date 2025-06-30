dfafb1f757556fcbe0dfd08cb06e63d30937a7e3
SheriffKule <zeljko225@yahoo.com>
2025-06-30 16:17:57 +0200

    Fractals Generator V1 & V2

--- widgets_script/fractalsGeneratorV2.js
+++ widgets_script/fractalsGeneratorV2.js
@@ -7,20 +7,20 @@ window.addEventListener('load', function () {
 
   ctx.fillStyle = 'green';
   ctx.lineCap = 'round';
-  ctx.shadowColor = 'rgba(0, 0, 0, 0,7)';
+  ctx.shadowColor = 'rgba(0,0,0,0.7)';
   ctx.shadowOffsetX = 10;
-  ctx.shadowOffsetY = 5;
-  ctx.shadowBlur = 10;
+  ctx.shadowOffsetY = 10;
+  ctx.shadowBlur = 20;
 
-  let size = canvas.width < canvas.height ? canvas.width * 0.25 : canvas.height * 0.25;
-  const maxLevel = 4;
-  const branches = 2;
+  let size = canvas.width < canvas.height ? canvas.width * 0.1 : canvas.height * 0.1;
+  const maxLevel = 10;
+  const branches = 1;
 
-  let sides = 5;
-  let scale = 0.5;
-  let spread = 0.1;
+  let sides = 10;
+  let scale = 0.85;
+  let spread = -0.2;
   let color = 'hsl(' + Math.random() * 360 + ', 100%, 50%)';
-  let lineWidth = Math.floor(Math.random() * 20 + 10);
+  let lineWidth = 30;
 
   const randomizeButton = document.getElementById('randomizeButton');
   const resetButton = document.getElementById('resetButton');
@@ -40,17 +40,19 @@ window.addEventListener('load', function () {
     updateSliders();
     drawFractal();
   });
+  let pointX = 0;
+  let pointY = size;
 
   function drawBranch(level) {
     if (level > maxLevel) return;
     ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(size, 0);
+    ctx.moveTo(pointX, pointY);
+    ctx.bezierCurveTo(0, size * spread * -3, size * 5, size * 10 * spread, 0, 0);
     ctx.stroke();
 
     for (let i = 0; i < branches; i++) {
       ctx.save();
-      ctx.translate(size - (size / branches) * i, 0);
+      ctx.translate(pointX, pointY);
       ctx.scale(scale, scale);
 
       ctx.save();
@@ -58,13 +60,11 @@ window.addEventListener('load', function () {
       drawBranch(level + 1);
       ctx.restore();
 
-      ctx.save();
-      ctx.rotate(-spread);
-      drawBranch(level + 1);
-      ctx.restore();
-
       ctx.restore();
     }
+    ctx.beginPath();
+    ctx.arc(-size / 2, 0, 40, 0, Math.PI * 2);
+    ctx.fill();
   }
 
   function drawFractal() {
@@ -72,10 +72,12 @@ window.addEventListener('load', function () {
     ctx.save();
     ctx.lineWidth = lineWidth;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
     ctx.translate(canvas.width / 2, canvas.height / 2);
 
     for (let i = 0; i < sides; i++) {
-      ctx.rotate((Math.PI * 2) / sides);
+      ctx.scale(0.95, 0.95);
+      ctx.rotate((Math.PI * 6) / sides);
       drawBranch(0);
     }
     ctx.restore();
@@ -86,11 +88,10 @@ window.addEventListener('load', function () {
   drawFractal();
 
   function randomizeFractal() {
-    sides = Math.floor(Math.random() * 7 + 2);
-    scale = Math.random() * 0.2 + 0.4;
-    spread = Math.random() * 2.9 + 0.1;
+    sides = Math.floor(Math.random() * 18 + 2);
+    spread = Math.random() * 0.6 - 0.3;
     color = 'hsl(' + Math.random() * 360 + ', 100%, 50%)';
-    lineWidth = Math.floor(Math.random() * 20 + 10);
+    lineWidth = Math.floor(Math.random() * 30 + 20);
   }
 
   randomizeButton.addEventListener('click', function () {
@@ -100,18 +101,18 @@ window.addEventListener('load', function () {
   });
 
   function resetFractal() {
-    sides = 5;
-    scale = 0.5;
-    spread = 0.7;
+    sides = 15;
+    scale = 0.85;
+    spread = 0.2;
     color = 'hsl(40, 100%, 50%)';
-    lineWidth = 15;
+    lineWidth = 30;
   }
 
-  resetButton.addEventListener('click', function() {
+  resetButton.addEventListener('click', function () {
     resetFractal();
     updateSliders();
     drawFractal();
-  })
+  });
 
   function updateSliders() {
     slider_spread.value = spread;
@@ -120,6 +121,17 @@ window.addEventListener('load', function () {
     label_sides.innerText = 'Sides: ' + sides;
   }
   updateSliders();
+
+  window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    size = canvas.width < canvas.height ? canvas.width * 0.1 : canvas.height * 0.1;
+    ctx.shadowColor = 'rgba(0,0,0,0.7)';
+    ctx.shadowOffsetX = 10;
+    ctx.shadowOffsetY = 10;
+    ctx.shadowBlur = 10;
+    drawFractal();
+  });
 });
 
 function updateYear() {
