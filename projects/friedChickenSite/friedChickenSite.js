dbc4864d2f9217b637ee04bd78a1de28e8d2dc00
SheriffKule <zeljko225@yahoo.com>
2025-07-08 11:36:15 +0200

    Fried Chicken part 2

--- projects/friedChickenSite/friedChickenSite.js
+++ projects/friedChickenSite/friedChickenSite.js
@@ -22,3 +22,14 @@ const linkAction = () => {
 }
 
 navLink.forEach((link) => link.addEventListener('click', linkAction));
+
+const scrollHeader = () => {
+  const header = document.getElementById('header');
+  if (this.scrollY >= 80) {
+    header.classList.add('scroll-header');
+  } else {
+    header.classList.remove('scroll-header');
+  }
+}
+
+window.addEventListener('scroll', scrollHeader);
