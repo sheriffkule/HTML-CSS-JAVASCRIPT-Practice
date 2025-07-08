48b72a141fb36b24e23f927fb7bcbee2175224ac
SheriffKule <zeljko225@yahoo.com>
2025-07-08 18:50:20 +0200

    Fried Chicken part 4

--- projects/friedChickenSite/friedChickenSite.js
+++ projects/friedChickenSite/friedChickenSite.js
@@ -19,7 +19,7 @@ const navLink = document.querySelectorAll('.nav__link');
 const linkAction = () => {
   const navMenu = document.getElementById('nav-menu');
   navMenu.classList.remove('show-menu');
-}
+};
 
 navLink.forEach((link) => link.addEventListener('click', linkAction));
 
@@ -30,6 +30,34 @@ const scrollHeader = () => {
   } else {
     header.classList.remove('scroll-header');
   }
-}
+};
 
 window.addEventListener('scroll', scrollHeader);
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+};
+
+window.addEventListener('scroll', scrollUp);
+
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute('id');
+    const sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+    if (scrollDown >= sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionClass.classList.add('active-link');
+    } else {
+      sectionClass.classList.remove('active-link');
+    }
+  });
+};
+
+window.addEventListener('scroll', scrollActive);
