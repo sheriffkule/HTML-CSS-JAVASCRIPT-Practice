65dc8c11fb364beada40411c9689b4c965727668
SheriffKule <zeljko225@yahoo.com>
2025-08-06 21:26:52 +0200

    Number Guessing Game part 1

---
+++ widgets_script/guessNumber.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const containerEl = document.querySelector('.container');
+const btnPlayEl = document.querySelector('.btn_again');
+const btnChckEl = document.querySelector('.btn_check');
+const hideNumEl = document.querySelector('.hide_num');
+const msgEl = document.querySelector('.message');
+const inputNumEl = document.querySelector('.input_number');
+const highScoreEl = document.querySelector('.high_score');
+const score = document.querySelector('.score');
