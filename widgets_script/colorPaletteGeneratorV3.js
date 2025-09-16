471ff6f6da2b704a85b126837b02e256ab588334
SheriffKule <zeljko225@yahoo.com>
2025-09-16 21:36:40 +0200

    Ultimate Color Palette Generator part 4

--- widgets_script/colorPaletteGeneratorV3.js
+++ widgets_script/colorPaletteGeneratorV3.js
@@ -88,6 +88,139 @@ function generateShadesPalette(hsl, count) {
   return palette;
 }
 
-let hsl = [155, 55, 55];
-let palette = generateShadesPalette(hsl, 6);
-console.log(palette);
+function generateTetradicPalette(hsl, count) {
+  const palette = [];
+  let [hue, saturation, lightness] = hsl;
+
+  for (let i = 0; i < count; i++) {
+    let newHue = hue + 90 * i;
+    if (newHue > 360) {
+      newHue -= 360 * Math.floor(newHue / 360);
+    }
+
+    palette.push([newHue, saturation, lightness]);
+  }
+
+  return palette;
+}
+
+function generateSquarePalette(hsl, count) {
+  const palette = [];
+  let [hue, saturation, lightness] = hsl;
+
+  for (let i = 0; i < count; i++) {
+    let newHue = hue + 60 * i;
+    if (newHue > 360) {
+      newHue -= 360 * Math.floor(newHue / 360);
+    }
+
+    palette.push([newHue, saturation, lightness]);
+  }
+
+  return palette;
+}
+
+function generateRelatedColorPalette(hsl, count) {
+  const palette = [];
+  let [hue, saturation, lightness] = hsl;
+
+  palette.push([hue, (saturation + 20) % 100, lightness]);
+  palette.push([hue, (saturation - 20) % 100, lightness]);
+  palette.push([hue, saturation, (lightness + 20) % 100]);
+  palette.push([hue, saturation, (lightness - 20) % 100]);
+  palette.push([(hue + 20) % 360, saturation, lightness]);
+  palette.push([(hue - 20) % 360, saturation, lightness]);
+
+  for (let i = palette.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [palette[i], palette[j]] = [palette[j], palette[i]];
+  }
+
+  return palette;
+}
+
+function generatePalette(hsl, type, count) {
+  switch (type) {
+    case 'analogous':
+      return generateAnalogousPalette(hsl, count);
+    case 'monochromatic':
+      return generateMonochromaticPalette(hsl, count);
+    case 'triadic':
+      return generateTriadicPalette(hsl, count);
+    case 'compound':
+      return generateCompoundPalette(hsl, count);
+    case 'shades':
+      return generateShadesPalette(hsl, count);
+    case 'tetradic':
+      return generateTetradicPalette(hsl, count);
+    case 'square':
+      return generateSquarePalette(hsl, count);
+    case 'related':
+      return generateRelatedColorPalette(hsl, count);
+  }
+}
+
+function generatePaletteHtml(type, container) {
+  let color = currentColor;
+  let count = currentCount;
+  const hsl = getHslFromColor(color);
+}
+
+function getHslFromColor(color) {
+  let hsl;
+  if (isValidColor(color)) {
+    let tempDiv = document.createElement('div');
+    tempDiv.style.color = color;
+    document.body.appendChild(tempDiv);
+
+    let style = window.getComputedStyle(tempDiv, null);
+    let rgb = style.getPropertyValue('color');
+    document.body.removeChild(tempDiv);
+
+    rgb = removeRGB(rgb);
+
+    hsl = rgbToHsl(rgb);
+  }
+}
+
+function isValidColor(color) {
+  return CSS.supports('color', color);
+}
+
+function removeRGB(rgb) {
+  return rgb.replace('rgb(', '').replace(')', '').split(', ');
+}
+
+function rgbToHsl(rgb) {
+  let r = parseInt(rgb[0]) / 255;
+  let g = parseInt(rgb[1]) / 255;
+  let b = parseInt(rgb[2]) / 255;
+
+  let cMax = Math.max(r, g, b);
+  let cMin = Math.min(r, g, b);
+  let delta = cMax - cMin;
+  let h = 0;
+  let s = 0;
+  let l = (cMax + cMin) / 2;
+
+  if (delta === 0) {
+    h = 0;
+    s = 0;
+  } else if (cMax === r) {
+    h = ((g - b) / delta) % 6;
+  } else if (cMax === g) {
+    h = (b - r) / delta + 2;
+  } else {
+    h = (r - g) / delta + 4;
+  }
+
+  h = Math.round(h * 60);
+  if (h < 0) h += 360;
+
+  if (delta !== 0) {
+    s = Math.round((delta / (1 - Math.abs(2 * l - 1))) * 100);
+  }
+  l = Math.round(l * 100);
+
+  return [h, s, l];
+}
