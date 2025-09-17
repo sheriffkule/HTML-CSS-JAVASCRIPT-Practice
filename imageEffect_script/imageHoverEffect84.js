14e559894d3f63b231b9723b5f444b9eb939cb73
SheriffKule <zeljko225@yahoo.com>
2025-09-17 23:02:21 +0200

    Image Slicing Effect part 2 Finished

--- imageEffect_script/imageHoverEffect84.js
+++ imageEffect_script/imageHoverEffect84.js
@@ -6,10 +6,15 @@ canvas.width = 460;
 canvas.height = 460;
 
 class Cell {
-  constructor(effect, x, y) {
+  constructor(effect, x, y, index) {
     this.effect = effect;
     this.x = x;
     this.y = y;
+    this.index = index;
+    this.positionX = this.effect.width * 0.5
+    this.positionY = this.effect.height;
+    this.speedX;
+    this.speedY;
     this.width = this.effect.cellWidth;
     this.height = this.effect.cellHeight;
     this.image = document.getElementById('projectImage');
@@ -17,6 +22,12 @@ class Cell {
     this.slideY = 0;
     this.vx = 0;
     this.vy = 0;
+    this.ease = 0.01;
+    this.friction = 0.9;
+    this.randomize = Math.random() * 50 + 2;
+    setTimeout(() => {
+        this.start();
+    }, this.index * 10);
   }
 
   draw(context) {
@@ -26,29 +37,40 @@ class Cell {
       this.y + this.slideY,
       this.width,
       this.height,
-      this.x,
-      this.y,
+      this.positionX,
+      this.positionY,
       this.width,
       this.height
     );
-    context.strokeRect(this.x, this.y, this.width, this.height);
+    context.strokeRect(this.positionX, this.positionY, this.width, this.height);
+  }
+
+  start() {
+    this.speedX = (this.x - this.positionX) / this.randomize;
+    this.speedY = (this.y - this.positionY) / this.randomize;
   }
 
   update() {
+    if (Math.abs(this.speedX) > 0.01 || Math.abs(this.speedY) > 0.01) {
+      this.speedX = (this.x - this.positionX) / this.randomize;
+      this.speedY = (this.y - this.positionY) / this.randomize;
+      this.positionX += this.speedX;
+      this.positionY += this.speedY;
+    }
+
     const dx = this.effect.mouse.x - this.x;
     const dy = this.effect.mouse.y - this.y;
     const distance = Math.hypot(dx, dy);
 
     if (distance < this.effect.mouse.radius) {
+      const angle = Math.atan2(dy, dx);
       const force = distance / this.effect.mouse.radius;
-      this.vx = force;
-      this.vy = force;
-    } else {
-      this.vx = 0;
-      this.vy = 0;
+      this.vx = force * Math.cos(angle);
+      this.vy = force * Math.sin(angle);
     }
-    this.slideX += this.vx;
-    this.slideY += this.vy;
+
+    this.slideX += (this.vx *= this.friction) - this.slideX * this.ease;
+    this.slideY += (this.vy *= this.friction) - this.slideY * this.ease;
   }
 }
 
@@ -64,24 +86,26 @@ class Effect {
     this.mouse = {
       x: undefined,
       y: undefined,
-      radius: 100,
+      radius: 150,
     };
 
-    this.canvas.addEventListener('mousemove', (e) => {
+    this.canvas.addEventListener('mousemove', e => {
       this.mouse.x = e.offsetX;
       this.mouse.y = e.offsetY;
     });
 
-    this.canvas.addEventListener('mousemove', (e) => {
+    this.canvas.addEventListener('mouseleave', e => {
       this.mouse.x = undefined;
       this.mouse.y = undefined;
     });
   }
 
   createGrid() {
+    let index = 0;
     for (let y = 0; y < this.height; y += this.cellHeight) {
       for (let x = 0; x < this.width; x += this.cellWidth) {
-        this.imageGrid.push(new Cell(this, x, y));
+        index++;
+        this.imageGrid.unshift(new Cell(this, x, y, index));
       }
     }
   }
@@ -97,6 +121,7 @@ class Effect {
 const effect = new Effect(canvas);
 
 function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
   effect.render(ctx);
   requestAnimationFrame(animate);
 }
