24f672e697a7a278ffa2e3169e6505854f15706b
SheriffKule <zeljko225@yahoo.com>
2025-09-26 13:33:49 +0200

    Drawing App part 2

---
+++ widgets_script/drawingApp.js
@@ -0,0 +1,6 @@
+/** @type {HTMLCanvasElement} */
+
+const canvas = document.querySelector('canvas')
+const toolBtns = document.querySelectorAll('.tool')
+const fillColor = document.querySelector('#fillColor')
+const sizeSlider = document.querySelector('#sizeSlider')
