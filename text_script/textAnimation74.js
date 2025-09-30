8e614435b94cf569dba085afa55a392f9d922a6b
SheriffKule <zeljko225@yahoo.com>
2025-09-30 17:07:03 +0200

    Particles Text part 1

---
+++ text_script/textAnimation74.js
@@ -0,0 +1,85 @@
+/** @type {HTMLCanvasElement} */
+
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let particleArray = [];
+
+const mouse = {
+  x: null,
+  y: null,
+  radius: 150,
+};
+
+window.addEventListener('mousemove', function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  mouse.radius = 250;
+});
+
+ctx.fillStyle = 'white';
+ctx.font = '50px Verdana';
+ctx.fillText('A', 0, 0);
+const data = ctx.getImageData(0, 0, 100, 100);
+
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 5;
+    this.baseX = this.x;
+    this.baseY = this.y;
+    this.density = Math.random() * 30 + 1;
+  }
+
+  draw() {
+    ctx.fillStyle = 'orangered';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  update() {
+    let dx = mouse.x - this.x;
+    let dy = mouse.y - this.y;
+    let distance = Math.hypot(dx, dy);
+    let forceDirectionX = dx / distance;
+    let forceDirectionY = dy / distance;
+    let maxDistance = mouse.radius;
+    let force = (maxDistance - distance) / maxDistance;
+    let directionX = forceDirectionX * force * this.density;
+    let directionY = forceDirectionY * force * this.density;
+    if (distance < 200) {
+      this.x += forceDirectionX * 3;
+      this.y += forceDirectionY * 2;
+    } else {
+      this.size = 5;
+    }
+  }
+}
+
+function init() {
+  particleArray = [];
+
+  for (let i = 0; i < 200; i++) {
+    let x = Math.random() * canvas.width;
+    let y = Math.random() * canvas.height;
+    particleArray.push(new Particle(x, y));
+  }
+}
+
+init();
+console.log(particleArray);
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].draw();
+    particleArray[i].update();
+  }
+  requestAnimationFrame(animate);
+}
+
+animate();
