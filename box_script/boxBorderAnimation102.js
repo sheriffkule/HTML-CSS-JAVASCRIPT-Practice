1fa0174d2b63af3463c3e0d94a5c344c78027562
SheriffKule <zeljko225@yahoo.com>
2025-10-10 15:41:51 +0200

    Scroll To Reveal Animation

---
+++ box_script/boxBorderAnimation102.js
@@ -0,0 +1,42 @@
+for (let i = 0; i < 60; i++) {
+  let box = document.createElement('div');
+  box.classList.add('box');
+  document.querySelector('.container').appendChild(box);
+}
+
+let randomColorBlock = document.querySelectorAll('.box');
+
+function addColor() {
+  randomColorBlock.forEach((e) => {
+    e.style.background = randomColor();
+  });
+}
+
+function randomColor() {
+  let chars = '1234567890abcdef';
+  let colorLength = 6;
+  let color = '';
+
+  for (let i = 1; i <= colorLength; i++) {
+    let randomColors = Math.floor(Math.random() * chars.length);
+    color += chars.substring(randomColors, randomColors + 1);
+  }
+
+  return '#' + color;
+}
+
+addColor();
+
+let boxes = document.querySelectorAll('.box');
+
+function scrollTrigger() {
+  boxes.forEach((box) => {
+    if (box.offsetTop < window.scrollY) {
+      box.classList.add('active');
+    } else {
+      box.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', scrollTrigger);
