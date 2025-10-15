d7e5b3ef8752d7e6b8b3ec11d2206ce6b1c31cb9
SheriffKule <zeljko225@yahoo.com>
2025-10-15 11:02:08 +0200

    Captcha Generator App part 1

---
+++ widgets_script/captchaGenerator.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const inputDisableEl = document.querySelector('.disable');
+const inputEnableEl = document.querySelector('.captcha');
+const btnRefreshEl = document.querySelector('.btn-icon');
+const messageEl = document.querySelector('.message');
+const btnSubmitEl = document.querySelector('.btn-submit');
