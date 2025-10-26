5705407677948335d884d9121533ad474c9e3ca8
SheriffKule <zeljko225@yahoo.com>
2025-10-26 16:02:58 +0100

    Animated Portfolio Website 2

--- projects/portfolio10/portfolio10.js
+++ projects/portfolio10/portfolio10.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const addEventOnElements = function (elements, eventType, callback) {
-  for (let i = 0; (i = elements.length); i++) {
+  for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener(eventType, callback);
   }
 };
@@ -12,3 +12,26 @@ window.addEventListener('load', function () {
   loadingElement.classList.add('loaded');
   document.body.classList.remove('active');
 });
+
+const [navTogglers, navLinks, navbar, overlay] = [
+  document.querySelectorAll('[data-nav-toggler]'),
+  document.querySelectorAll('[data-nav-link]'),
+  document.querySelector('[data-navbar]'),
+  document.querySelector('[data-overlay]'),
+];
+
+const toggleNav = function () {
+  navbar.classList.toggle('active');
+  overlay.classList.toggle('active');
+  document.body.classList.toggle('active');
+};
+
+addEventOnElements(navTogglers, 'click', toggleNav);
+
+const closeNav = function () {
+  navbar.classList.remove('active');
+  overlay.classList.remove('active');
+  document.body.classList.remove('active');
+};
+
+addEventOnElements(navLinks, 'click', closeNav);
