a0ae02be066a41d073ea080b78110ff4d2315a0d
SheriffKule <zeljko225@yahoo.com>
2025-10-26 18:07:06 +0100

    Animated Portfolio Website 3

--- projects/portfolio10/portfolio10.js
+++ projects/portfolio10/portfolio10.js
@@ -35,3 +35,55 @@ const closeNav = function () {
 };
 
 addEventOnElements(navLinks, 'click', closeNav);
+
+const header = document.querySelector('[data-header]');
+
+const activeElementOnScroll = function () {
+  if (window.scrollY > 50) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
+};
+
+window.addEventListener('scroll', activeElementOnScroll);
+
+const letterBoxes = document.querySelectorAll('[data-letter-effect]');
+
+let activeLetterBoxIndex = 0;
+let lastActiveLetterBoxIndex = 0;
+let totalLetterBoxDelay = 0;
+
+const setLetterEffect = function () {
+  for (let i = 0; i < letterBoxes.length; i++) {
+    let letterAnimationDelay = 0;
+    const letters = letterBoxes[i].textContent.trim();
+
+    letterBoxes[i].textContent = '';
+
+    for (let j = 0; j < letters.length; j++) {
+      const span = document.createElement('span');
+      span.style.animationDelay = `${letterAnimationDelay}s`;
+      span.classList.add(i === activeLetterBoxIndex ? 'in' : 'out');
+
+      span.textContent = letters[j];
+      if (letters[j] === ' ') span.classList.add('space');
+      letterBoxes[i].appendChild(span);
+
+      if (j >= letters.length - 1) break;
+
+      letterAnimationDelay += 0.1;
+    }
+
+    if (i === activeLetterBoxIndex) totalLetterBoxDelay = Number(letterAnimationDelay.toFixed(2));
+    letterBoxes[i].classList.toggle('active', i === lastActiveLetterBoxIndex);
+  }
+
+  setTimeout(function () {
+    lastActiveLetterBoxIndex = activeLetterBoxIndex;
+    activeLetterBoxIndex >= letterBoxes.length - 1 ? (activeLetterBoxIndex = 0) : activeLetterBoxIndex++;
+
+    setLetterEffect();
+  }, totalLetterBoxDelay * 1000 + 3000);
+};
+window.addEventListener('load', setLetterEffect);
