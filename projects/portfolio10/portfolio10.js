a87f259ed80a9ada4c2d79d16dea03e88067b206
SheriffKule <zeljko225@yahoo.com>
2025-10-26 14:19:03 +0100

    Animated Portfolio Website 1

---
+++ projects/portfolio10/portfolio10.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0; (i = elements.length); i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+const loadingElement = document.querySelector('[data-loading]');
+
+window.addEventListener('load', function () {
+  loadingElement.classList.add('loaded');
+  document.body.classList.remove('active');
+});
