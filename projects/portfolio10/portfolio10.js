5210450a17ad35fcf91d786e2d80fa6ec9d97231
SheriffKule <zeljko225@yahoo.com>
2025-10-27 19:18:06 +0100

    Animated Portfolio Website 8

--- projects/portfolio10/portfolio10.js
+++ projects/portfolio10/portfolio10.js
@@ -104,3 +104,21 @@ window.addEventListener('scroll', function () {
     backTopBtn.classList.remove('show');
   }
 });
+
+const revealElements = document.querySelectorAll('[data-reveal]');
+
+const scrollReveal = function () {
+  for (let i = 0; i < revealElements.length; i++) {
+    const elementIsInScreen = revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.15;
+
+    if (elementIsInScreen) {
+      revealElements[i].classList.add('revealed');
+    } else {
+      revealElements[i].classList.remove('revealed');
+    }
+  }
+};
+
+window.addEventListener('scroll', scrollReveal);
+
+scrollReveal();
