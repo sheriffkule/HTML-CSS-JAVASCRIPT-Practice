116a141dba062fb48d9a86b254d3a718221f79f5
SheriffKule <zeljko225@yahoo.com>
2025-11-11 16:00:58 +0100

    Medical Website 8

--- projects/medicalWebsite/medicalWebsite.js
+++ projects/medicalWebsite/medicalWebsite.js
@@ -45,6 +45,84 @@ const swiperPrices = new Swiper('.prices__swiper', {
   },
 });
 
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+};
+
+window.addEventListener('scroll', scrollUp);
+
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute('id');
+    const sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionClass.classList.add('active-link');
+    } else {
+      sectionClass.classList.remove('active-link');
+    }
+  });
+};
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-fill';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? 'dark' : 'light');
+const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? 'ri-moon-fill' : 'ri-sun-fill');
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'ri-moon-fill' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+window.addEventListener('scroll', scrollActive);
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '150px',
+  duration: 2000,
+  reset: true,
+});
+console.log(sr);
+
+sr.reveal(`.home__content`, { origin: 'bottom' });
+sr.reveal(`.home__info`, { origin: 'bottom', delay: 800 });
+sr.reveal(`.home__data`, { delay: 1400 });
+sr.reveal(`.home__button`, { origin: 'left', delay: 1800 });
+
+sr.reveal(`.delivery__data`, { origin: 'right' });
+sr.reveal(`.delivery__content`, { origin: 'left', delay: 600 });
+sr.reveal(`.delivery__img`, { delay: 1200 });
+
+sr.reveal(`.about__data, .contact__map`, { origin: 'left' });
+sr.reveal(`.about__img, .contact__data`, { origin: 'right' });
+
+sr.reveal(`.prices__box`);
+sr.reveal(`.prices__swiper`, { origin: 'bottom', delay: 600 });
+
+sr.reveal(`.gallery__image`, { interval: 200 });
+
+sr.reveal(`.footer__container`);
+
 function updateYear() {
   const currentYear = new Date().getFullYear();
   const yearElement = document.getElementById('year');
