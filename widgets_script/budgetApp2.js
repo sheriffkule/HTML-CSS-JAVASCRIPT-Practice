2c200ec647af70ab4d4905262fb418ecf1a4bc17
SheriffKule <zeljko225@yahoo.com>
2025-11-15 20:12:37 +0100

    Budget App 3

--- widgets_script/budgetApp2.js
+++ widgets_script/budgetApp2.js
@@ -2,7 +2,7 @@
 
 const errorMessage = document.querySelector('.error_message');
 const budgetInputEl = document.querySelector('.budget_input');
-const expenseDelEl = document.querySelector('.expenses_input');
+const expenseDescEl = document.querySelector('.expenses_input');
 const expenseAmountEl = document.querySelector('.expenses_amount');
 const tableRecordEl = document.querySelector('.table_data');
 const cardsContainer = document.querySelector('.cards');
@@ -27,28 +27,122 @@ function btnEvents() {
 
   btnExpensesCal.addEventListener('click', (e) => {
     e.preventDefault();
-    console.log('expenses');
+    expensesFun();
   });
 }
 
 // Calling Btns Events
 document.addEventListener('DOMContentLoaded', btnEvents);
 
+// Expenses Function
+function expensesFun() {
+  let expensesDescValue = expenseDescEl.value;
+  let expenseAmountValue = expenseAmountEl.value;
+
+  if (expensesDescValue == '' || expensesDescValue == '' || budgetInputEl < 0) {
+    showErrorMessage('Please Enter Expenses Description and Expense Amount!');
+  } else {
+    let amount = parseInt(expenseAmountValue);
+    expenseAmountEl.value = '';
+    expenseDescEl.value = '';
+
+    // store the value inside object
+    let expenses = {
+      id: itemId,
+      title: expensesDescValue,
+      amount: amount,
+    };
+    itemId++;
+    itemList.push(expenses);
+
+    // add expenses inside the HTML Page
+    addExpenses(expenses);
+    showBalance();
+  }
+}
+
+// Add Expenses
+function addExpenses(expensesPara) {
+  const html = ` <ul class="tbl_tr_content">
+      <li data-id="${expensesPara.id}">${expensesPara.id + 1}</li>
+      <li>${expensesPara.title}</li>
+      <li><span>$</span>${expensesPara.amount}</li>
+      <li>
+        <button type="submit" class="btn_edit">Edit</button>
+        <button type="submit" class="btn_delete">Delete</button>
+      </li>
+    </ul>
+    `;
+
+  tableRecordEl.insertAdjacentHTML('beforeend', html);
+
+  // edit
+  const editBtn = document.querySelectorAll('btn_edit');
+  const delBtn = document.querySelectorAll('.btn_delete');
+  const content_id = document.querySelectorAll('.tbl_tr_content');
+
+  // btn edit event
+  editBtn.forEach((btnEdit) => {
+    btnEdit.addEventListener('click', (el) => {
+      let id;
+
+      content_id.forEach((ids) => {
+        id = ids.firstElementChild.dataset.id;
+      });
+
+      let element = el.target.parentElement.parentElement;
+      element.remove();
+
+      let expenses = itemList.filter(function (item) {
+        return item.id == id;
+      });
+
+      expenseDescEl.value = expenses[0].title;
+      expenseAmountEl.value = expenses[0].amount;
+
+      let temp_list = itemList.filter(function(item) {
+        return item.id !== id;
+      })
+    });
+  });
+}
 // Budget Function
 function budgetFun() {
   const budgetValue = budgetInputEl.value;
 
   if (budgetValue === '' || budgetValue < 0) {
-      showErrorMessage()
+    showErrorMessage('Please Enter Budget Amount | More Than 0!');
   } else {
+    budgetCardEl.textContent = budgetValue;
+    budgetInputEl.value = '';
+    showBalance();
   }
 }
 
-function showErrorMessage() {
-  errorMessage.style.visibility = 'visible';
-  errorMessage.style.height = 'auto';
+function showBalance() {
+  const expenses = totalExpenses();
+  const total = parseInt(budgetCardEl.textContent) - expenses;
+  balanceCardEl.textContent = total;
+}
+
+function totalExpenses() {
+  let total = 0;
+
+  if (itemList.length > 0) {
+    total = itemList.reduce(function (acc, curr) {
+      acc += curr.amount;
+      return acc;
+    }, 0);
+  }
+  expensesCardEl.textContent = total;
+
+  return total;
+}
+
+function showErrorMessage(message) {
+  errorMessage.innerHTML = `<p>${message}</p>`;
+  errorMessage.classList.add('error');
   setTimeout(() => {
-    errorMessage.style.visibility = 'hidden';
-    errorMessage.style.height = 0;
+    errorMessage.classList.remove('error');
   }, 2500);
 }
