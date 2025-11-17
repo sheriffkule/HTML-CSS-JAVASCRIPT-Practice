cc4ef58439effe3a450c25ad2beabb0da99d684e
SheriffKule <zeljko225@yahoo.com>
2025-11-17 21:13:25 +0100

    Card  & Quick Action

---
+++ box_script/boxBorderAnimation108.js
@@ -0,0 +1,11 @@
+const showMenu = (btnId, menuId) => {
+  const btn = document.getElementById(btnId);
+  const menu = document.getElementById(menuId);
+
+  btn.addEventListener('click', () => {
+    menu.classList.toggle('show-menu');
+    btn.classList.toggle('show-icon');
+  });
+};
+
+showMenu('card-btn', 'card-menu');
