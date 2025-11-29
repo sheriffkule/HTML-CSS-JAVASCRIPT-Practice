8749a4f841ae8313a2c5581cea5cb45a3e48eabb
SheriffKule <zeljko225@yahoo.com>
2025-11-29 19:18:17 +0100

    Virtual World Website 7

--- projects/virtulaWorldWebsite/virtulaWorldWebsite.js
+++ projects/virtulaWorldWebsite/virtulaWorldWebsite.js
@@ -2,4 +2,12 @@ let swiper = new Swiper('.brands_Swiper', {
     slidesPerView: 4,
     spaceBetween: 20,
     loop: true,
+    autoplay: true,
+})
+
+let swiperTest = new Swiper('.testimonialSwiper', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: true,
 })
