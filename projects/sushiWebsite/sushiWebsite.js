ce4d2b317feee865eca3de6a8943bf03d63245d3
SheriffKule <zeljko225@yahoo.com>
2026-02-02 21:15:24 +0100

    Sushi Restaurant 3

--- projects/sushiWebsite/sushiWebsite.js
+++ projects/sushiWebsite/sushiWebsite.js
@@ -24,3 +24,10 @@ const linkAction = () => {
 };
 
 navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+// Change header styles
+const scrollHeader = () => {
+  const header = document.getElementById('header');
+  this.scrollY >= 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header');
+};
+window.addEventListener('scroll', scrollHeader);
