65e6bf2665c6538a015f5a3af3431029d87bc123
SheriffKule <zeljko225@yahoo.com>
2026-05-29 14:54:03 +0200

    Portfolio Website 4

--- projects/portfolio11/portfolio11.js
+++ projects/portfolio11/portfolio11.js
@@ -23,3 +23,34 @@ const linkAction = () => {
   navMenu.classList.remove('show-menu');
 };
 navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+// Home text circular
+const homeText = document.getElementById('home-text');
+const letters = homeText.textContent.trim().split('');
+const angleStep = 360 / letters.length;
+
+homeText.textContent = '';
+
+// Iterates through each letter
+letters.forEach((char, i) => {
+  const span = document.createElement('span');
+  span.textContent = char;
+  span.style.transform = `rotate(${i * angleStep}deg)`;
+  homeText.appendChild(span);
+});
+
+// Home typed JS
+const typedHome = new Typed('#home-typed', {
+  strings: ['Web Developer', 'Freelancer', 'Frontend Master'],
+  typeSpeed: 100,
+  backSpeed: 60,
+  backDelay: 2000,
+  loop: true,
+});
+
+// Change header styles
+const scrollHeader = () => {
+  const header = document.getElementById('header');
+  this.scrollY >= 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header');
+};
+window.addEventListener('scroll', scrollHeader);
