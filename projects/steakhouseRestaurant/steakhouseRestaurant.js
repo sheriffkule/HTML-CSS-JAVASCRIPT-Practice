6cfb6d1dc9a958f9d2512c84d898487bdd7e30fb
SheriffKule <zeljko225@yahoo.com>
2026-07-14 20:28:00 +0200

    Steakhouse Restaurant 7

--- projects/steakhouseRestaurant/steakhouseRestaurant.js
+++ projects/steakhouseRestaurant/steakhouseRestaurant.js
@@ -27,3 +27,29 @@ const scrollHeader = () => {
   this.scrollY >= 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header');
 };
 window.addEventListener('scroll', scrollHeader);
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+};
+window.addEventListener('scroll', scrollUp);
+
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute('id');
+    const sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    }
+  });
+};
+window.addEventListener('scroll', scrollActive);
