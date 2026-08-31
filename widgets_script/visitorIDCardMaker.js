4c810827ec275e3034845590dcdff8d107c00ba4
sheriffkule <zeljko225@yahoo.com>
2026-08-31 15:22:38 +0200

    Visitor ID Card Maker 2

---
+++ widgets_script/visitorIDCardMaker.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // DOM Elements
+  const card = document.getElementById('card');
+  const cardTitleInput = document.getElementById('cardTitle');
+  const cardTitleDisplay = document.getElementById('cardTitleDisplay');
+  const nameInput = document.getElementById('name');
+  const companyInput = document.getElementById('company');
+  const purposeInput = document.getElementById('purpose');
+  const purposeDisplay = document.getElementById('purposeDisplay');
+});
