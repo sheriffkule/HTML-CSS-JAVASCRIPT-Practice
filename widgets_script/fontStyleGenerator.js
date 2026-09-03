2a8960ecd04607ac2827d596a41c04ee89ac32a3
sheriffkule <zeljko225@yahoo.com>
2026-09-03 18:51:49 +0200

    Font Style Generator 10

--- widgets_script/fontStyleGenerator.js
+++ widgets_script/fontStyleGenerator.js
@@ -553,4 +553,33 @@ document.addEventListener('DOMContentLoaded', function () {
     presetName.value = '';
     loadPresets();
   }
+
+  // Changing colors on input type range track
+  document.querySelectorAll('input[type="range"]').forEach((input) => {
+    const updateTrack = () => {
+      const min = parseFloat(input.min) || 0;
+      const max = parseFloat(input.max) || 100;
+      const value = parseFloat(input.value);
+      const ratio = Math.min(Math.max((value - min) / (max - min), 0), 1);
+      const val = ratio * 100;
+
+      input.style.backgroundImage = `linear-gradient(to right, var(--primary) 0%, var(--success) ${val}%, var(--surface) ${val}%, var(--surface) 100%)`;
+    };
+    input.addEventListener('input', updateTrack);
+    updateTrack();
+  });
+
+  // Update year in footer
+  function updateYear() {
+    const currentYear = new Date().getFullYear();
+    const yearElement = document.getElementById('year');
+
+    if (!yearElement) {
+      console.error('Year element not found');
+      return;
+    }
+    yearElement.setAttribute('datetime', currentYear.toString());
+    yearElement.textContent = currentYear.toString();
+  }
+  updateYear();
 });
