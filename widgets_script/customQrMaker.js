df5be72bb5f61654904295365980e6a63931f38e
sheriffkule <zeljko225@yahoo.com>
2026-09-17 08:34:17 +0200

    Custom QR Code Maker 4

--- widgets_script/customQrMaker.js
+++ widgets_script/customQrMaker.js
@@ -130,4 +130,19 @@ document.addEventListener('DOMContentLoaded', function () {
       updateQrDetails();
     }
   }
+
+  // Changing colors on input type range track
+  document.querySelectorAll('input[type="range"]').forEach((input) => {
+    const updateTrack = () => {
+      const min = parseFloat(input.min) || 0;
+      const max = parseFloat(input.max) || 100;
+      const value = parseFloat(input.value);
+      const ratio = Math.min(Math.max((value - min) / (max - min), 0), 1);
+      const val = ratio * 100;
+
+      input.style.backgroundImage = `linear-gradient(to right, var(--success) 0%, var(--primary) ${val}%, #a0a0c0 ${val}%, #a0a0c0 100%)`;
+    };
+    input.addEventListener('input', updateTrack);
+    updateTrack();
+  });
 });
